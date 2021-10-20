e1b6d0efc0f9d2609c809b2dc415eda65054f684
The Sharp Ninja <ninja@thesharp.ninja>
2021-10-20 07:47:50 -0500

    Add project files.

---
+++ ResearchMaster/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace ResearchMaster
+{
+	public partial class App
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ ResearchMaster/MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using ResearchMaster.Data;
+
+//#if WINDOWS
+//using App = ResearchMaster.WinUI.App;
+//#else
+//using App = ResearchMaster.App;
+//#endif
+
+namespace ResearchMaster
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+#if !WINDOWS
+                .UseMauiApp<App>()
+#endif
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView();
+
+            return builder.Build();
+        }
+    }
+}
